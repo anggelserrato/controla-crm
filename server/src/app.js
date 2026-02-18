2bbc02f0107b08a9e606493131c43a025156ec68
Angel Serrato <serrato.anggel@gmail.com>
2026-02-18 18:39:33 -0500

    feat: add morgan http logging middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import { errorHandler } from './middlewares/error.middleware.js';
@@ -9,9 +10,9 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({
