9f9381255a349b242a975abeeb3544eb3924147c
Angel Serrato <serrato.anggel@gmail.com>
2026-02-19 18:44:27 -0500

    feat(frontend): setup react vite tailwindcss shadcn - resolved #9

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>ControlaCRM</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
